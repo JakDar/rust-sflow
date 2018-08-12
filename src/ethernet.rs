d3feb7ac93245408c99aad37d0480d9ef4b4ecbc
Jakub Darul <daruljakub@gmail.com>
2018-08-12 17:02:13 +0200

    fixed fields in Ipv4 packet

--- src/ethernet.rs
+++ src/ethernet.rs
@@ -1,4 +1,4 @@
-use ipaddress::{IPAddress, decode_ipv4, decode_ipv6};
+use ipaddress::{decode_ipv4, IPAddress};
 use std::io::SeekFrom;
 
 add_decoder! {
@@ -30,13 +30,19 @@ pub struct Ipv4Packet {
     pub header_checksum: u16,
     pub source_addr: IPAddress,
     pub dst_addr: IPAddress,
-    //todo - handle non-empty options
+    pub options_were_present: bool,
+
     pub content: IpContent,
 }
 
 impl ::utils::Decodeable for Ipv4Packet {
     fn read_and_decode(stream: &mut ::types::ReadSeeker) -> ::std::result::Result<Ipv4Packet, ::error::Error> {
         let version_and_ihl: u8 = ::utils::Decodeable::read_and_decode(stream)?;
+
+
+        let version = version_and_ihl >> 4;
+        let header_length = version_and_ihl % (1 << 4);
+
         let tos_and_ecn: u8 = ::utils::Decodeable::read_and_decode(stream)?;
         let total_length: u16 = ::utils::Decodeable::read_and_decode(stream)?;
         let row1: u32 = ::utils::Decodeable::read_and_decode(stream)?;
@@ -46,9 +52,17 @@ impl ::utils::Decodeable for Ipv4Packet {
         let source_addr = decode_ipv4(stream)?;
         let dst_addr = decode_ipv4(stream)?;
 
+        let options_were_present = header_length > 5;
+
+        if options_were_present {
+            let option_len_in_bytes = 4 * (header_length - 5);
+            stream.seek(SeekFrom::Current(option_len_in_bytes as i64))?;
+        }
+
+
         let packet: Ipv4Packet = Ipv4Packet {
-            version: version_and_ihl >> 4,
-            header_length: version_and_ihl % 1 << 4,
+            version,
+            header_length,
             tos: tos_and_ecn >> 1,
             ecn: tos_and_ecn % 2,
             total_length,
@@ -58,6 +72,7 @@ impl ::utils::Decodeable for Ipv4Packet {
             header_checksum,
             source_addr,
             dst_addr,
+            options_were_present: false,
             content: IpContent {},
         };
         stream.seek(SeekFrom::Current((packet.total_length - packet.header_length as u16 * 4u16) /*(todo*/ as i64))?;
